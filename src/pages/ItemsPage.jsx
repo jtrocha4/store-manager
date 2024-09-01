70b7b312a66a11455d8e10f00f4749aef9be393a
jtrocha4 <jtrocha04@gmail.com>
2024-09-01 14:31:09 -0500

    update(pages): ItemsPage

--- src/pages/ItemsPage.jsx
+++ src/pages/ItemsPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Table from '../components/Table'
+import Modal from '../components/Modal'
 
 function ItemsPage () {
   return (
     <div>
       <h1 className='text-3xl font-bold tracking-tight text-gray-900'>Items</h1>
-      <section className='mx-auto my-7 py-7'>
-        <Table />
+      <section className='mx-auto'>
+        <article className='mt-7'>
+          <Modal label='Nuevo item' background='bg-green-600' />
+        </article>
+        <article className='mt-5'>
+          <Table />
+        </article>
       </section>
     </div>
   )
